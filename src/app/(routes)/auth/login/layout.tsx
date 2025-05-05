7bb104d05fead8a442dcfc666b952e7cca1e1033
Ouamouno <jeankelouaouamoun71@gmail.com>
2025-05-05 19:37:25 +0000

    suppresion des doublons dans les pages

---
+++ src/app/(routes)/auth/login/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+import { Suspense } from "react";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Tabali - Connectez-vous avec des prestataires de services en Guinée",
+  description: "Tabali connecte particuliers et prestataires (ménage, plomberie, électricité, etc.) en Guinée pour tous vos besoins quotidiens.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
+      >
+        <main className="flex-1"><Suspense>{children}</Suspense></main>
+      </body>
+    </html>
+  );
+}
